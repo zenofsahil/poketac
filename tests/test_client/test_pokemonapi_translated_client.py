05682f0e538f4637a9ad21de80786574f664e105
Sahil <sahilsingh90@gmail.com>
2022-04-06 19:34:24 +0100

    Move endpoint tests to client test modules and use test client fixture

--- tests/test_client/test_pokemonapi_translated_client.py
+++ tests/test_client/test_pokemonapi_translated_client.py
@@ -1,2 +1,4 @@
-def test_pokemonapitranslated_client():
-    assert True
+def test_pokemonapitranslated_client(client):
+    response = client.get("/pokemon/translated/pikachu")
+    assert response.status_code == 200
+    assert response.json() == [{"name": "pikachu"}]
