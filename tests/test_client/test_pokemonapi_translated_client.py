c447d8d5fe841ed6a377814ad0c0132269802a43
Sahil <sahilsingh90@gmail.com>
2022-04-06 20:19:41 +0100

    Add response payload structure test for /translated api

--- tests/test_client/test_pokemonapi_translated_client.py
+++ tests/test_client/test_pokemonapi_translated_client.py
@@ -1,4 +1,10 @@
-def test_pokemonapitranslated_client(client):
+def test_payload_keys(client):
     response = client.get("/pokemon/translated/pikachu")
     assert response.status_code == 200
-    assert response.json() == [{"name": "pikachu"}]
+
+    payload = response.json()[0]
+
+    assert "name" in payload
+    assert "description" in payload
+    assert "habitat" in payload
+    assert "isLegendary" in payload
