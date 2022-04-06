9c3ece78a43e5349e7d4a62983db99a8e2a97ead
Sahil <sahilsingh90@gmail.com>
2022-04-06 04:30:53 +0100

    Add env vars to settings object

--- pokeapi/config.py
+++ pokeapi/config.py
@@ -3,10 +3,12 @@ from typing import Any, Dict, List, Optional, Union
 
 from pydantic import AnyHttpUrl, BaseSettings, PostgresDsn, validator
 
-
 class Settings(BaseSettings):
     PROJECT_NAME: str
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    
+    BASE_URL: str = ""
+    ENDPOINT: str = ""
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
