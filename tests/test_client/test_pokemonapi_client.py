8c90008355ba2a2885a09824fabda5d7630e181c
Sahil <sahilsingh90@gmail.com>
2022-04-06 20:12:07 +0100

    Add /pokemon api payload test

--- tests/test_client/test_pokemonapi_client.py
+++ tests/test_client/test_pokemonapi_client.py
@@ -1,3 +1,18 @@
+import pytest
+
+def test_payload_keys(client):
+    response = client.get("/pokemon/pikachu")
+    assert response.status_code == 200
+
+    payload = response.json()[0]
+
+    assert "name" in payload
+    assert "description" in payload
+    assert "habitat" in payload
+    assert "isLegendary" in payload
+
+
+@pytest.mark.skip(reason="Should belong in integration tests")
 def test_pokemonapi_client(client):
     response = client.get("/pokemon/pikachu")
     assert response.status_code == 200
