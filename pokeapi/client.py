fa3c27036396513c95a48787657bf4d117bc85d8
Sahil <sahilsingh90@gmail.com>
2022-04-06 19:01:57 +0100

    Add get_description staticmethod to PokemonClient

--- pokeapi/client.py
+++ pokeapi/client.py
@@ -1,4 +1,5 @@
 import requests
+from typing import Optional
 from urllib.parse import urljoin
 
 class PokemonClient:
@@ -14,6 +15,25 @@ class PokemonClient:
         print(url)
         r = requests.get(url)
         return r
+    @staticmethod
+    def get_description(self, json_content: dict) -> Optional[str]:
+        """
+        Extract the printable english description from the `json_content`
+        """
+        descriptions = json_content.get('flavour_text_entries')
+
+        if descriptions is not None and len(descriptions) > 0:
+            for description in descriptions:
+                language = description.get('language')
+                if (
+                    language is not None and
+                    language.get('name') is not None and
+                    language.get('name') == 'en' and
+                    description.get('flavor_text') is not None
+                ):
+                    return description.get('flavor_text')
+        return None
+
 
 class PokemonTranslatedClient:
     def __init__(self, base_url, endpoint):
