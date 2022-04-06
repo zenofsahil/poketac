4366fa82c7d8e4241e39fbfe50bfd0f5b1695dc9
Sahil <sahilsingh90@gmail.com>
2022-04-06 22:52:54 +0100

    [feature/caching] Add test for testing caching functionality

--- tests/test_client/test_pokemonapi_client.py
+++ tests/test_client/test_pokemonapi_client.py
@@ -1,4 +1,7 @@
 import pytest
+from pokeapi.client import requests
+from pokeapi.main import pokemon_client
+from datetime import datetime
 
 def test_payload_keys(client):
     response = client.get("/pokemon/pikachu")
@@ -10,6 +13,26 @@ def test_payload_keys(client):
     assert "description" in payload
     assert "habitat" in payload
     assert "isLegendary" in payload
+
+
+def test_pokemonapi_client_caching(monkeypatch):
+
+    def patched(arg):
+        return datetime.utcnow().timestamp()
+
+    monkeypatch.setattr(requests, 'get', patched)
+    data1 = pokemon_client.fetch_url('https://sample.url')
+    # The return value for 'https://sample.url' has now been cached. So if we
+    # were to call it again, it should be the same as when we first called it.
+    data2 = pokemon_client.fetch_url('https://sample.url')
+
+    # The value for 'https://another.url' has not been cached and will be newly
+    # computed.
+    data3 = pokemon_client.fetch_url('https://another.url')
+
+    assert data1 == data2
+    assert data1 != data3
+    assert data2 != data3
 
 
 @pytest.mark.skip(reason="Should belong in integration tests")
