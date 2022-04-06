554d5d11e0f95093e605106886b38193cf1c6fbc
Sahil <sahilsingh90@gmail.com>
2022-04-06 20:11:04 +0100

    Implement get_pokemon_info method for PokemonClient

--- pokeapi/client.py
+++ pokeapi/client.py
@@ -8,15 +8,30 @@ class PokemonClient:
         self.endpoint = endpoint
 
     def get_pokemon_info(self, name):
-        return { 'name': name }
-
-    def _get_pokemon_info(self, name):
         url = urljoin(self.base_url, f'{self.endpoint}/{name}')
-        print(url)
         r = requests.get(url)
-        return r
+        if r.ok and r.json() is not None:
+            
+            response = {
+                'name' : r.json().get('name', name),
+                'description' : self.get_description(r.json()),
+                'habitat' : self.get_habitat(r.json()),
+                'isLegendary' : r.json().get('is_legendary', False)
+            }
+            return response
+        else:
+            raise Exception
+
     @staticmethod
-    def get_description(self, json_content: dict) -> Optional[str]:
+    def get_habitat(json_content: dict) -> Optional[str]:
+        """
+        Extract the habitat value from the `json_content`
+        """
+        habitat = json_content.get('habitat')
+        return habitat.get('name') if habitat else None
+            
+    @staticmethod
+    def get_description(json_content: dict) -> Optional[str]:
         """
         Extract the printable english description from the `json_content`
         """
