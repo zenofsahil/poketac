7f3577820a95ec2a9478b7573479aeeaa7870665
Sahil <sahilsingh90@gmail.com>
2022-04-07 17:12:35 +0100

    [feature/tests] Add fixture for retrieving pokemon data as json from files

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,17 @@
 import pytest
+import json
 from fastapi.testclient import TestClient
 from pokeapi.main import app
-
 
 @pytest.fixture(name='client')
 def test_client():
     return TestClient(app)
+
+@pytest.fixture(name="pokemon_data")
+def pokemon_data():
+    def extract_data(name):
+        assert name in ["crobat", "ditto", "heatran", "pikachu"]
+        f = open(f"tests/test_data/pokeapi_responses/{name}.json", 'r').read()
+        data = json.loads(f)
+        return data
+    return extract_data
