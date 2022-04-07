95004da45acc6bc17f3590087271ab2fb7bd6c1c
Sahil <sahilsingh90@gmail.com>
2022-04-08 00:46:39 +0100

    [feature/error_handling] Refactor: Separate out PokemonClient and TranslateClient. ...
    This will enable easier error handling as errors from the different api's can be decoupled
    and thrown in different places in the application. This also implements an abstract base class
    which can be reimplemented for adding new clients to our application

--- pokeapi/client.py
+++ pokeapi/client.py
@@ -1,5 +1,6 @@
 import logging
 import requests
+from abc import ABC, abstractmethod
 from functools import lru_cache
 from typing import Optional
 from urllib.parse import urljoin
@@ -8,21 +9,20 @@ from pokeapi.utils import remove_nonprintable_chars
 
 logger = logging.getLogger(__name__)
 
-class PokemonClient:
-    def __init__(self, base_url: str, endpoint: str, translate_api: str):
+
+class Client(ABC):
+
+    @abstractmethod
+    def fetch_url(url: str) -> dict:
+        return
+
+
+class PokemonClient(Client):
+    def __init__(self, base_url: str, endpoint: str):
         self.base_url = base_url
         self.endpoint = endpoint
-        self.translate_api = translate_api
 
-    def get_pokemon_info(self, name: str, translate: bool = False) -> dict:
-        if translate:
-            logger.info('Translate API called for pokemon %s', name)
-            return self.get_pokemon_info_translated(name)
-
-        logger.info('Basic API called for pokemon %s', name)
-        return self.get_pokemon_info_basic(name)
-
-    def get_pokemon_info_basic(self, name: str) -> dict:
+    def get_pokemon_info(self, name: str) -> dict:
         """
         Get basic information for the pokemon as specified by `name`
         """
@@ -68,14 +68,43 @@ class PokemonClient:
         if r.ok and r.json() is not None:
             return r.json()
 
-        raise Exception
+        raise 
 
+    @staticmethod
+    def get_habitat(json_content: dict) -> Optional[str]:
+        """
+        Extract the habitat value from the `json_content`
+        """
+        habitat_dict = json_content.get('habitat')
+        if habitat_dict:
+            return habitat_dict.get('name')
+        logger.debug('No valid habitat value found')
+        return None
+            
+    @staticmethod
+    def get_description(json_content: dict) -> Optional[str]:
+        """
+        Extract the printable english description from the `json_content`
+        """
+        descriptions = json_content.get('flavor_text_entries')
 
-    def get_pokemon_info_translated(self, name: str) -> dict:
-        basic_info = self.get_pokemon_info_basic(name)
-        logger.debug('Retrieved basic information')
-        translated_info = self.translate_description(basic_info)
-        return translated_info
+        if descriptions is not None and len(descriptions) > 0:
+            for description in descriptions:
+                language = description.get('language')
+                if (
+                    language is not None and
+                    language.get('name') is not None and
+                    language.get('name') == 'en' and
+                    description.get('flavor_text') is not None
+                ):
+                    return remove_nonprintable_chars(description.get('flavor_text', ''))
+        
+        logger.debug('No valid description found.')
+        return None
+
+class TranslateClient:
+    def __init__(self, translate_url: str):
+        self.translate_url = translate_url
 
     def translate_description(self, basic_info: dict) -> dict:
         """
@@ -91,7 +120,7 @@ class PokemonClient:
         translation_kind = self.get_translation_kind(basic_info)
         logger.debug('Attempting "%s" translation.', translation_kind)
 
-        translation_url = urljoin(self.translate_api, f'translate/{translation_kind}')
+        translation_url = urljoin(self.translate_url, f'translate/{translation_kind}')
         request_data = {'text': basic_info.get('description')}
 
         prepared_request = PreparedRequest()
@@ -137,34 +166,17 @@ class PokemonClient:
         return translation_kind
 
     @staticmethod
-    def get_habitat(json_content: dict) -> Optional[str]:
+    @lru_cache
+    def fetch_url(url: str) -> dict:
         """
-        Extract the habitat value from the `json_content`
+        This method is duplicated from PokemonClient and not commonly inherited so that 
+        1. The caches for the 2 apis can live separately.
+        2. Client specific exceptions can be thrown in a more straightforward way.
         """
-        habitat_dict = json_content.get('habitat')
-        if habitat_dict:
-            return habitat_dict.get('name')
-        logger.debug('No valid habitat value found')
-        return None
-            
-    @staticmethod
-    def get_description(json_content: dict) -> Optional[str]:
-        """
-        Extract the printable english description from the `json_content`
-        """
-        descriptions = json_content.get('flavor_text_entries')
+        logger.debug('Result not present in cache. Requesting data from network.')
+        r = requests.get(url)
 
-        if descriptions is not None and len(descriptions) > 0:
-            for description in descriptions:
-                language = description.get('language')
-                if (
-                    language is not None and
-                    language.get('name') is not None and
-                    language.get('name') == 'en' and
-                    description.get('flavor_text') is not None
-                ):
-                    return remove_nonprintable_chars(description.get('flavor_text', ''))
-        
-        logger.debug('No valid description found.')
-        return None
+        if r.ok and r.json() is not None:
+            return r.json()
 
+        raise 
