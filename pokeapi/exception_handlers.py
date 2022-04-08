edbfe348ca5e4fd01e2b446344a6e3fe872ea3b3
Sahil <sahilsingh90@gmail.com>
2022-04-08 03:08:14 +0100

    [feature/error_handling] Add exception handlers for non HTTP api errors

--- pokeapi/exception_handlers.py
+++ pokeapi/exception_handlers.py
@@ -21,3 +21,13 @@ async def pokemon_exception_handler(request: Request, exc: PokemonAPIHTTPExcepti
 async def pokemon_exception_handler(request: Request, exc: TranslationAPIHTTPException):
     logger.error(exc.detail)
     return PlainTextResponse(exc.detail, status_code=exc.status_code)
+
+@app.exception_handler(TranslationAPIException)
+async def pokemon_exception_handler(request: Request, exc: TranslationAPIException):
+    logger.error(f"{exc.__class__.__name__}")
+    return PlainTextResponse("Internal Error", status_code=500)
+
+@app.exception_handler(PokemonAPIException)
+async def pokemon_exception_handler(request: Request, exc: PokemonAPIException):
+    logger.error(f"{exc.__class__.__name__}")
+    return PlainTextResponse("Internal Error", status_code=500)
