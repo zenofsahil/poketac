a047c474535d4b4a9ae25d4b37b7efd7eb7bbc36
Sahil <sahilsingh90@gmail.com>
2022-04-09 21:41:08 +0100

    Bugfix: Rename exception handlers to have unique names

--- pokeapi/exception_handlers.py
+++ pokeapi/exception_handlers.py
@@ -13,21 +13,21 @@ from pokeapi.exceptions import (
 logger = logging.getLogger(__name__)
     
 @app.exception_handler(PokemonAPIHTTPException)
-async def pokemon_exception_handler(request: Request, exc: PokemonAPIHTTPException):
+async def pokemon_api_http_exception_handler(request: Request, exc: PokemonAPIHTTPException):
     logger.error(exc.detail)
     return PlainTextResponse(exc.detail, status_code=exc.status_code)
 
 @app.exception_handler(TranslationAPIHTTPException)
-async def pokemon_exception_handler(request: Request, exc: TranslationAPIHTTPException):
+async def pokemon_translation_api_http__exception_handler(request: Request, exc: TranslationAPIHTTPException):
     logger.error(exc.detail)
     return PlainTextResponse(exc.detail, status_code=exc.status_code)
 
 @app.exception_handler(TranslationAPIException)
-async def pokemon_exception_handler(request: Request, exc: TranslationAPIException):
+async def pokemon_translation_api_exception_handler(request: Request, exc: TranslationAPIException):
     logger.error(f"{exc.__class__.__name__}")
     return PlainTextResponse("Internal Error", status_code=500)
 
 @app.exception_handler(PokemonAPIException)
-async def pokemon_exception_handler(request: Request, exc: PokemonAPIException):
+async def pokemon_api_exception_handler(request: Request, exc: PokemonAPIException):
     logger.error(f"{exc.__class__.__name__}")
     return PlainTextResponse("Internal Error", status_code=500)
